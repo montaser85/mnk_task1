8fc148840b4770e613265e08d4617146bf05559b
montaser85 <montamcc@gmail.com>
2023-03-18 15:40:52 -0700

    board moves initial logic

--- src/components/NewGameBoard.svelte
+++ src/components/NewGameBoard.svelte
@@ -2,7 +2,8 @@
     import { scaleLinear, scaleOrdinal } from "d3-scale";
     import { each } from "svelte/internal";
     import { onMount } from "svelte";
-    import {box_select_store} from "../store.js";
+    import {box_select_store, move_number, first_array, second_array, goes_first} from "../store.js";
+    //import pred_data from "../prediction_results.json";
     let GB_X1=180;
     let GB_Y1=0;
     let boardBoxWidth=80;
@@ -20,9 +21,13 @@
     let row_names=["1","2","3","4"]
     boxArray=Array.apply(null, {length: boxNum}).map(Number.call, Number)
     let box;
-    let move_num=0;
+    let move_num;
     let box_select;
     let board_id;
+    let board_components=[];
+    let max_move=9;
+    let goes_first_array=[];
+    let goes_second_array=[];
     box_select_store.subscribe((data) => {
          box_select = data
     })
@@ -32,12 +37,66 @@
     function box_reupdate(){
         box_select_store.set(null);
     }
-    console.log(box_select)
+
+    move_number.subscribe((data) => {
+         move_num = data
+    })
+    function move_increment(){
+        move_number.update(n =>n+1 );
+        BoxElementUpdate(move_num);
+    }
+    function move_decrement(){
+        move_number.update(n =>n-1 );
+        BoxElementUpdate(move_num);
+    }
+    
+    let i=0;
+    function BoxElementUpdate(move_num){
+        let first_array_i=0;
+        let second_array_i=0;
+        goes_first_array=[];
+        goes_second_array=[];
+        console.log(move_num)
+        //goes_first_array.push(move_number);
+        if(move_num>0){
+            
+            for (i=1;i<=move_num;i++){
+                if(i%2==1){
+                    goes_first_array.push(first_array[first_array_i]);
+                    //goes_first_array.push(2);
+                    first_array_i++;
+                }
+                else{
+                    goes_second_array.push(second_array[second_array_i]);
+                    second_array_i++;
+                }
+            }
+        }
+        console.log(goes_first_array);
+        console.log(goes_second_array);
+    }
+
+    // console.log(first_array);
+    // onMount(async () => {
+    //     const fetched=await fetch("../data.json");
+    //     instances = (await fetched.json()).board-data;
+    //     console.log(instances);
+    // });
+
+    // onMount(async () => {
+	// 	const fetched = await fetch('../static/data/data.json');
+	// 	const inst = (await fetched.json()).board_data;
+    //     instances=inst;
+    //     console.log(instances);
+
+
+	// });
+    
 </script>
 <!-- <main> -->
-    <!-- <div>
-        counter value: {$box_select_store}
-    </div> -->
+    <div>
+        counter value: {goes_first_array}
+    </div>
     <div id="GameBoard">
         <svg id="svgBoard" width="100%" height="100%" viewBox="0 0 {viewBoxWidth} {viewBoxHeight}"
         preserveAspectRatio="xMinYMid meet">
@@ -49,13 +108,8 @@
                     {board_id=(row_index*9)+column_index}
                     <rect class="BoardBox {box_select!=null ? box_select==box ? "this-box-selected":"this-box-not-selected":"pre-box"}" x={GB_X1+(column_index*boardBoxHeight)} y={GB_Y1+(row_index*boardBoxHeight)} width={boardBoxWidth} height={boardBoxHeight}
                         on:mouseenter={()=>{
-<<<<<<< HEAD
                             box_update(box);
 
-=======
-                            box_select=box;
-                            console.log("selected")
->>>>>>> c8309e2386903c9cbedb3bc9bd13ff2e7ceb69fe
                         }}
                         on:mouseleave={()=>{
                             box_reupdate();
@@ -77,32 +131,46 @@
                         <line class="GridLine" x1={GB_X1+(column_names.indexOf(column_name)*boardBoxWidth)} x2={GB_X1+(column_names.indexOf(column_name)*boardBoxWidth)} y1={GB_Y1} y2={GB_Y1+(4*boardBoxHeight)}/>
                     {/if}
                 {/each}
-<<<<<<< HEAD
-=======
-
->>>>>>> c8309e2386903c9cbedb3bc9bd13ff2e7ceb69fe
                 <!-- adding buttons -->
                 <!-- prior move button -->
-                <g>
+                <g
+                on:click={()=>{
+                    if(move_num>=1){
+                        move_decrement();
+                    }
+                }}
+                on:keypress={()=>{
+                  if(move_num>=1){
+                        move_decrement();
+                    }
+                }}
+                >
                     <rect class="BoardButton" x={GB_X1+(1.5*boardBoxHeight)} y={GB_Y1+(4.2*boardBoxHeight)} rx="10" ry="10" width={boardBoxWidth*1.5} height={boardBoxHeight*0.8}/>
                     <text class="ButtonLabels" x={GB_X1+(1.7*boardBoxHeight)} y={GB_Y1+(4.5*boardBoxHeight)}>Prior Move</text>
                     <!-- <image class="imagebox" x={GB_X1+(1.7*boardBoxHeight)} y={GB_Y1+20+(4.2*boardBoxHeight)} width=70 height=50 href="static/images/arrow_back.png" alt=""/> -->
                     <image class="imagebox" x={GB_X1+10+(1.7*boardBoxHeight)} y={GB_Y1+25+(4.2*boardBoxHeight)} width=60 height=40 href="static/images/arrow_back.png" alt=""/>
                 </g>
-                <g>
+                <g
+                on:click={()=>{
+                    if(move_num<max_move){
+                        move_increment();
+                    }
+                }}
+                on:keypress={()=>{
+                    if(move_num<=max_move){
+                        move_increment();
+                    }
+                }}
+                >
                     <!-- Next move button  -->
-                    <rect class="BoardButton" x={GB_X1+(6*boardBoxHeight)} y={GB_Y1+(4.2*boardBoxHeight)} rx="10" ry="10" width={boardBoxWidth*1.5} height={boardBoxHeight*0.8}/>
+                    <rect class="BoardButton" x={GB_X1+(6*boardBoxHeight)} y={GB_Y1+(4.2*boardBoxHeight)} rx="10" ry="10" width={boardBoxWidth*1.5} height={boardBoxHeight*0.8}
+                    />
                     <text class="ButtonLabels" x={GB_X1+(6.2*boardBoxHeight)} y={GB_Y1+(4.5*boardBoxHeight)}>Next Move</text>
                     <!-- <image class="imagebox" x={GB_X1+(6.2*boardBoxHeight)} y={GB_Y1+20+(4.2*boardBoxHeight)} width=70 height=50 href="static/images/arrow_for.png" alt=""/> -->
                     <image class="imagebox" x={GB_X1+10+(6.2*boardBoxHeight)} y={GB_Y1+25+(4.2*boardBoxHeight)} width=60 height=40 href="static/images/arrow_for.png" alt=""/>
                 </g>
-<<<<<<< HEAD
                 
                 <!-- adding text of the board -->
-=======
-
-
->>>>>>> c8309e2386903c9cbedb3bc9bd13ff2e7ceb69fe
                 <text class="BoardLabels" x={GB_X1+(3.6*boardBoxHeight)} y={GB_Y1+(4.6*boardBoxHeight)}>Showing Move: {move_num}</text>
                 <text class="BoardLabels" x={GB_X1+(3.1*boardBoxHeight)} y={GB_Y1+(5.25*boardBoxHeight)}>Who moves next: </text>
             </g>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,49 @@
-import { writable } from "svelte/store";
-// const SelectionStore = writable([{
-//     id: 1,
-//     row_index: 2,
-//     column_index: 3
-// }]);
+import { readable, writable } from "svelte/store";
+import { onMount } from "svelte";
+import data_json from "../public/static/data/data.json";
+
 let box_select_store = writable(100);
+let move_number = writable(0);
+let data;
+let first_array=[];
+let second_array=[];
+let goes_first=[];
 
 export {box_select_store}
+export {move_number}
+export {first_array}
+export {second_array}
+export {goes_first}
 
-// export default SelectionStore;
+// onMount(async () => {
+//     data=await fetch('../JsonFiles/Game6.json');
+
+// });
+
+// data=await fetch('../JsonFiles/Game6.json');
+// console.log(data_json)
+// onMount(async () => {
+//     const fetched = await fetch('data.json');
+//     const inst = (await fetched.json()).board_data;
+//     instances=inst;
+//     console.log(instances);
+
+    
+// });
+
+if(data_json.board_data[0]["goes-first"]=="X"){
+    first_array=data_json.board_data[0]["x-moves"];
+    second_array=data_json.board_data[0]["o-moves"];
+    goes_first="X";
+}
+else{
+    first_array=data_json.board_data[0]["o-moves"];
+    second_array=data_json.board_data[0]["x-moves"];
+    goes_first="O";
+}
+
+// console.log(first_array);
+// console.log(second_array);
+
+
+
