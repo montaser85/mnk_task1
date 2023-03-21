46fd7a24084969b941731a53b5752835390f9f22
montaser85 <montamcc@gmail.com>
2023-03-21 11:48:00 -0700

    stt logic is done

--- src/components/ScoresThroughTime.svelte
+++ src/components/ScoresThroughTime.svelte
@@ -1,6 +1,6 @@
 <script>
-    import { scaleLinear, scaleOrdinal } from "d3-scale";
-    import {
+  import { scaleLinear, scaleOrdinal } from "d3-scale";
+  import {
     box_select_store,
     move_number,
     first_array,
@@ -11,53 +11,51 @@
     winner,
     max_move,
     btw_arrays,
-    btw_scores_arrays
+    btw_scores_arrays,
   } from "../store.js";
-    let y_axis_x1=80;
-    let y_axis_x2=80;
-    let y_axis_y1=50;
-    let y_axis_y2=320;
-    let y_diff=(y_axis_y2-y_axis_y1)/20;
-    let sttChartBoxWidth=30;
-    let sttChartBoxHeight=6;
-    let btw_x1=80;
-    let btw_x2=1100;
-    let btw_y1=340;
-    let btw_y2=340;
+  let y_axis_x1 = 80;
+  let y_axis_x2 = 80;
+  let y_axis_y1 = 50;
+  let y_axis_y2 = 320;
+  let y_diff = (y_axis_y2 - y_axis_y1) / 20;
+  let sttChartBoxWidth = 30;
+  let sttChartBoxHeight = 6;
+  let btw_x1 = 80;
+  let btw_x2 = 1100;
+  let btw_y1 = 340;
+  let btw_y2 = 340;
 
-    let arrow1_x1=btw_x1;
-    let arrow1_x2=btw_x1+10;
-    let arrow1_y1=btw_y1;
-    let arrow1_y2=btw_y1-10;
+  let arrow1_x1 = btw_x1;
+  let arrow1_x2 = btw_x1 + 10;
+  let arrow1_y1 = btw_y1;
+  let arrow1_y2 = btw_y1 - 10;
 
-    let arrow2_x1=btw_x1;
-    let arrow2_x2=btw_x1+10;
-    let arrow2_y1=btw_y1;
-    let arrow2_y2=btw_y1+10;
+  let arrow2_x1 = btw_x1;
+  let arrow2_x2 = btw_x1 + 10;
+  let arrow2_y1 = btw_y1;
+  let arrow2_y2 = btw_y1 + 10;
 
-    let arrow3_x1=btw_x2;
-    let arrow3_x2=btw_x2-10;
-    let arrow3_y1=btw_y1;
-    let arrow3_y2=btw_y1+10;
+  let arrow3_x1 = btw_x2;
+  let arrow3_x2 = btw_x2 - 10;
+  let arrow3_y1 = btw_y1;
+  let arrow3_y2 = btw_y1 + 10;
 
-    let arrow4_x1=btw_x2;
-    let arrow4_x2=btw_x2-10;
-    let arrow4_y1=btw_y1;
-    let arrow4_y2=btw_y1-10;
-    let xScaleTicks=[];
-    let xScaleNew;
-    xScaleNew=scaleLinear()
-			.domain([0,1]);
-    xScaleTicks=xScaleNew.ticks(21);
-    //console.log(xScaleTicks);
-    let xIndex;
-    let y_label_offsetX=45;
-    let y_label_offsetY=3;
-    let viewBoxWidth=1100;
-    let viewBoxHeight=390;
+  let arrow4_x1 = btw_x2;
+  let arrow4_x2 = btw_x2 - 10;
+  let arrow4_y1 = btw_y1;
+  let arrow4_y2 = btw_y1 - 10;
+  let xScaleTicks = [];
+  let xScaleNew;
+  xScaleNew = scaleLinear().domain([0, 1]);
+  xScaleTicks = xScaleNew.ticks(21);
+  let xIndex;
+  let y_label_offsetX = 45;
+  let y_label_offsetY = 3;
+  let viewBoxWidth = 1100;
+  let viewBoxHeight = 390;
 
-    let array_taken=0;
-    let box_list = [
+  let array_taken = 0;
+  let box_list = [
     "A1",
     "B1",
     "C1",
@@ -95,204 +93,276 @@
     "H4",
     "I4",
   ];
-let box_num;
-let btw_array_index=0;
-let i=0;
-let btw_arrays_taken=[];
-let btw_scores_arrays_taken=[];
-let boxArray=[];
-let box_select;
-let box_value;
-let btw_index;
-let k;
-let x_value;
-let score_value;
-let diff;
-let move_num;
-boxArray = Array.apply(null, { length: 36 }).map(Number.call, Number);
-let PositionScaleNew;
+  let box_num;
+  let btw_array_index = 0;
+  let i = 0;
+  let btw_arrays_taken = [];
+  let btw_scores_arrays_taken = [];
+  let boxArray = [];
+  let box_select;
+  let box_value;
+  let btw_index;
+  let k;
+  let x_value;
+  let score_value;
+  let diff;
+  let move_num;
+  boxArray = Array.apply(null, { length: 36 }).map(Number.call, Number);
+  let PositionScaleNew;
+  let checkLastChart;
 
-move_number.subscribe((data) => {
+  move_number.subscribe((data) => {
     move_num = data;
   });
 
-$:{
-    if(move_num>0){
-        btw_arrays_taken=[];
-        let limit=Math.ceil(move_num/2)
-    for (i=0;i<limit;i++){
-            btw_arrays_taken.push(btw_arrays[i])
-            btw_scores_arrays_taken.push(btw_scores_arrays[i])
-        }
+  $: {
+    if (move_num > 0) {
+      btw_arrays_taken = [];
+      let limit = Math.ceil(move_num / 2);
+      for (i = 0; i < limit; i++) {
+        btw_arrays_taken.push(btw_arrays[i]);
+        btw_scores_arrays_taken.push(btw_scores_arrays[i]);
+      }
     }
-        // console.log(btw_scores_arrays_taken[0][k][0]); 
-}
+  }
 
-box_select_store.subscribe((data) => {
+  box_select_store.subscribe((data) => {
     box_select = data;
-    if(box_select!=null){
-        box_select=box_list[box_select];
+    if (box_select != null) {
+      box_select = box_list[box_select];
     }
   });
-
-function box_update(box) {
+  function box_update(box) {
     box_select_store.update((n) => box);
   }
-  function box_reupdate() {
+  function box_reupdate(checkLastChart) {
     box_select_store.set(null);
   }
 
-function xValueUpdate(box_index,btw_index){
-    x_value=(y_axis_x1+5) + (btw_index* 2* sttChartBoxWidth)
+  function xValueUpdate(box_index, btw_index) {
+    x_value = y_axis_x1 + 5 + btw_index * 2 * sttChartBoxWidth;
     return x_value;
-}
-function yValueUpdate(box_char,btw_index){
-    score_value=btw_scores_arrays_taken[btw_index][box_char][0];
-    score_value=PositionScaleNew(score_value);
+  }
+  function yValueUpdate(box_char, btw_index) {
+    score_value = btw_scores_arrays_taken[btw_index][box_char][0];
+    score_value = PositionScaleNew(score_value);
     return score_value;
-    
-}
+  }
 
-PositionScaleNew=scaleLinear()
-			.domain([1,-1])
-            .range([50,320]);
-
+  PositionScaleNew = scaleLinear().domain([1, -1]).range([50, 320]);
 </script>
 
 <main>
-    <div id="chart">
-        <svg id="svgCharts" width="100%" height="100%" viewBox="0 0 {viewBoxWidth} {viewBoxHeight}" 
-        preserveAspectRatio="xMinYMid meet">
-        <g class="sttBoxes">
-            {#if move_num>0}
-                {#each btw_arrays_taken as btw_array,index}
-                        {#each btw_array as box,i}
-                            {#if i==0}
-                            {console.log("first Top")}
-                                <rect
-                                class="ChartBox {box_select == box
-                                ? 'this-top-box-selected'
-                                : 'this-top-box-not-selected'}"
-                                x={xValueUpdate(btw_array.indexOf(box),index)}
-                                y={yValueUpdate(box,index)}
-                                width={sttChartBoxWidth}
-                                height={sttChartBoxHeight}
-                                on:mouseenter={() => {
-                                    box_num=box_list.indexOf(box)
-                                    box_update(box_num);
-                                }}
-                                on:mouseleave={() => {
-                                box_reupdate();
-                                }}
-                                />
-                            {:else}
-                                <rect
-                                    class="ChartBox {box_select == box
-                                    ? 'this-box-selected'
-                                    : 'this-box-not-selected'}"
-                                    x={xValueUpdate(btw_array.indexOf(box),index)}
-                                    y={yValueUpdate(box,index)}
-                                    width={sttChartBoxWidth}
-                                    height={sttChartBoxHeight}
-                                    on:mouseenter={() => {
-                                        box_num=box_list.indexOf(box)
-                                        box_update(box_num);
-                                    }}
-                                    on:mouseleave={() => {
-                                    box_reupdate();
-                                    }}
-                                    />  
-                        {/if}
-                        {/each}
-                {/each}
+  <div id="chart">
+    <svg
+      id="svgCharts"
+      width="100%"
+      height="100%"
+      viewBox="0 0 {viewBoxWidth} {viewBoxHeight}"
+      preserveAspectRatio="xMinYMid meet"
+    >
+      <g class="sttBoxes">
+        {#if move_num > 0}
+          {#each btw_arrays_taken as btw_array, index}
+            {#if btw_arrays_taken.length == index + 1}
+              {#each btw_array as box, i}
+                {#if i == 0}
+                  <rect
+                    class="ChartBox {box_select == box
+                      ? 'this-top-box-selected'
+                      : 'this-top-box-not-selected'}"
+                    x={xValueUpdate(btw_array.indexOf(box), index)}
+                    y={yValueUpdate(box, index)}
+                    width={sttChartBoxWidth}
+                    height={sttChartBoxHeight}
+                    on:mouseenter={() => {
+                      box_num = box_list.indexOf(box);
+                      box_update(box_num);
+                    }}
+                    on:mouseleave={() => {
+                      box_reupdate();
+                    }}
+                  />
+                {:else}
+                  <rect
+                    class="ChartBox {box_select == box
+                      ? 'this-box-selected'
+                      : 'this-box-not-selected'}"
+                    x={xValueUpdate(btw_array.indexOf(box), index)}
+                    y={yValueUpdate(box, index)}
+                    width={sttChartBoxWidth}
+                    height={sttChartBoxHeight}
+                    on:mouseenter={() => {
+                      box_num = box_list.indexOf(box);
+                      box_update(box_num);
+                    }}
+                    on:mouseleave={() => {
+                      box_reupdate();
+                    }}
+                  />
+                {/if}
+              {/each}
+            {:else}
+              {#each btw_array as box, i}
+                {#if i == 0}
+                  <rect
+                    class="ChartBox {'this-top-box-not-selected'}"
+                    x={xValueUpdate(btw_array.indexOf(box), index)}
+                    y={yValueUpdate(box, index)}
+                    width={sttChartBoxWidth}
+                    height={sttChartBoxHeight}
+                  />
+                {:else}
+                  <rect
+                    class="ChartBox {'this-box-not-selected'}"
+                    x={xValueUpdate(btw_array.indexOf(box), index)}
+                    y={yValueUpdate(box, index)}
+                    width={sttChartBoxWidth}
+                    height={sttChartBoxHeight}
+                  />
+                {/if}
+              {/each}
             {/if}
-        </g>
+          {/each}
+        {/if}
+      </g>
 
-        <g id="ChartView">
-                <text class="ChartHeading" x={((btw_x2-btw_x1)/2)-y_label_offsetX} y={y_axis_y1-(5*y_label_offsetY)}>Scores through-time(TT)</text>
-                <line class="axis" x1={y_axis_x1} y1={y_axis_y1} x2={y_axis_x2} y2={y_axis_y2}/>
-                <line class="axis" x1={btw_x1} y1={btw_y1} x2={btw_x2} y2={btw_y2}/>
-                <!-- <line class="axis" x1={arrow1_x1} y1={arrow1_y1} x2={arrow1_x2} y2={arrow1_y2}/>
+      <g id="ChartView">
+        <text
+          class="ChartHeading"
+          x={(btw_x2 - btw_x1) / 2 - y_label_offsetX}
+          y={y_axis_y1 - 5 * y_label_offsetY}>Scores through-time(TT)</text
+        >
+        <line
+          class="axis"
+          x1={y_axis_x1}
+          y1={y_axis_y1}
+          x2={y_axis_x2}
+          y2={y_axis_y2}
+        />
+        <line class="axis" x1={btw_x1} y1={btw_y1} x2={btw_x2} y2={btw_y2} />
+        <!-- <line class="axis" x1={arrow1_x1} y1={arrow1_y1} x2={arrow1_x2} y2={arrow1_y2}/>
                 <line class="axis" x1={arrow2_x1} y1={arrow2_y1} x2={arrow2_x2} y2={arrow2_y2}/> -->
-                <line class="axis" x1={arrow3_x1} y1={arrow3_y1} x2={arrow3_x2} y2={arrow3_y2}/>
-                <line class="axis" x1={arrow4_x1} y1={arrow4_y1} x2={arrow4_x2} y2={arrow4_y2}/>
-                <text class="y_labels" x={y_axis_x1-y_label_offsetX} y={y_axis_y1+y_label_offsetY}>Win</text>
-                <text class="y_labels" x={y_axis_x1-y_label_offsetX} y={y_axis_y1+y_label_offsetY+(y_diff*10)}>Draw</text>
-                <text class="y_labels" x={y_axis_x1-y_label_offsetX} y={y_axis_y1+y_label_offsetY+(y_diff*20)}>Loss</text>
-                <text class="y_labels" x={((btw_x2-btw_x1)/2)} y={btw_y2+(10*y_label_offsetY)}>Time</text>
-                {#each xScaleTicks as tk}
-                    {xIndex=xScaleTicks.indexOf(tk)}
-                    {#if xIndex%5!=0}
-                        <line class="ThinLine" x1={btw_x1} y1={y_axis_y1+(xIndex*y_diff)} x2={btw_x2} y2={y_axis_y1+(xIndex*y_diff)}/>
-                    {:else if xIndex==10}
-                        <line class="axis" x1={btw_x1} y1={y_axis_y1+(xIndex*y_diff)} x2={btw_x2} y2={y_axis_y1+(xIndex*y_diff)}/>
-                    {:else if xIndex==5||xIndex==15}
-                        <line class="ThinAxis" x1={btw_x1} y1={y_axis_y1+(xIndex*y_diff)} x2={btw_x2} y2={y_axis_y1+(xIndex*y_diff)}/>
-                    {/if}
-                {/each}
-            </g>
-        </svg>
-    </div>
+        <line
+          class="axis"
+          x1={arrow3_x1}
+          y1={arrow3_y1}
+          x2={arrow3_x2}
+          y2={arrow3_y2}
+        />
+        <line
+          class="axis"
+          x1={arrow4_x1}
+          y1={arrow4_y1}
+          x2={arrow4_x2}
+          y2={arrow4_y2}
+        />
+        <text
+          class="y_labels"
+          x={y_axis_x1 - y_label_offsetX}
+          y={y_axis_y1 + y_label_offsetY}>Win</text
+        >
+        <text
+          class="y_labels"
+          x={y_axis_x1 - y_label_offsetX}
+          y={y_axis_y1 + y_label_offsetY + y_diff * 10}>Draw</text
+        >
+        <text
+          class="y_labels"
+          x={y_axis_x1 - y_label_offsetX}
+          y={y_axis_y1 + y_label_offsetY + y_diff * 20}>Loss</text
+        >
+        <text
+          class="y_labels"
+          x={(btw_x2 - btw_x1) / 2}
+          y={btw_y2 + 10 * y_label_offsetY}>Time</text
+        >
+        {#each xScaleTicks as tk}
+          {(xIndex = xScaleTicks.indexOf(tk))}
+          {#if xIndex % 5 != 0}
+            <line
+              class="ThinLine"
+              x1={btw_x1}
+              y1={y_axis_y1 + xIndex * y_diff}
+              x2={btw_x2}
+              y2={y_axis_y1 + xIndex * y_diff}
+            />
+          {:else if xIndex == 10}
+            <line
+              class="axis"
+              x1={btw_x1}
+              y1={y_axis_y1 + xIndex * y_diff}
+              x2={btw_x2}
+              y2={y_axis_y1 + xIndex * y_diff}
+            />
+          {:else if xIndex == 5 || xIndex == 15}
+            <line
+              class="ThinAxis"
+              x1={btw_x1}
+              y1={y_axis_y1 + xIndex * y_diff}
+              x2={btw_x2}
+              y2={y_axis_y1 + xIndex * y_diff}
+            />
+          {/if}
+        {/each}
+      </g>
+    </svg>
+  </div>
 </main>
 
 <style>
-    #chart{
-        display: flex;
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        
-    }
-    #svgCharts{
-        display: flex;
-        overflow: hidden;
-    }
-    #ChartView{
-        display: flex;
-        width: 100%;
-        height: 100%;
-    }
-    .axis{
-        stroke: black;
-        stroke-width: 2;
-    }
-    .ThinAxis{
-        stroke: black;
-        stroke-width: 1;
-    }
-    .ThinLine{
-        stroke: gray;
-        stroke-width: 0.5;
-    }
-    .y_labels{
-        font-weight: 500;
-    }
-    .ChartHeading{
-        font-weight: 500;
-        font-size: 2em;
-    }
-    .ChartBox.this-box-not-selected {
-    fill: #D4D3D3;
+  #chart {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  #svgCharts {
+    display: flex;
+    overflow: hidden;
+  }
+  #ChartView {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+  .axis {
+    stroke: black;
+    stroke-width: 2;
+  }
+  .ThinAxis {
+    stroke: black;
+    stroke-width: 1;
+  }
+  .ThinLine {
+    stroke: gray;
+    stroke-width: 0.5;
+  }
+  .y_labels {
+    font-weight: 500;
+  }
+  .ChartHeading {
+    font-weight: 500;
+    font-size: 2em;
+  }
+  .ChartBox.this-box-not-selected {
+    fill: #d4d3d3;
     /* stroke: black;
     stroke-width: 1; */
-
   }
-    .ChartBox.this-box-selected {
+  .ChartBox.this-box-selected {
     fill: yellow;
     stroke: black;
     stroke-width: 1;
-
   }
   .ChartBox.this-top-box-not-selected {
     fill: blue;
     /* stroke: black;
     stroke-width: 1; */
-
   }
-    .ChartBox.this-top-box-selected {
+  .ChartBox.this-top-box-selected {
     fill: yellow;
     stroke: black;
     stroke-width: 1;
-
   }
 </style>
